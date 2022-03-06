bfced87390b3cd38f62994e1e22c1938ce4e36bc
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-03-06 18:36:15 +0300

    added

---
+++ Task Method (1)/Task Method (1)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_Method__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Min(0));
+        }
+
+        static int Min(int nums)
+        {
+            int[] num = { 1, 2, 3, 4, 5 };
+            int min = num[0];
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (min > num[i])
+                {
+                    min = num[i];
+                }
+            }
+            return min ;
+        }
+
+
+
+    }
+}

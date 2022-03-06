248e539426688619595836fc29156d204118b341
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-03-06 21:13:47 +0300

    added

---
+++ Task Method(2)/Task Method(2)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_Method_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reqem daxil edin:");
+            int numbers = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("Bu rəqəmdə ədəd sayı:");
+            Console.WriteLine(NumLength(numbers));
+            
+
+
+
+
+        }
+
+        static int NumLength(int numbers)
+        {
+
+            int count = 0;         
+            while (numbers > 0)
+            {
+                numbers /= 10;
+                count++;
+            }
+            return count;
+        }
+    }
+}

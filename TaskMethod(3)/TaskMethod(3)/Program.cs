93600711e1bc772cbd08e047547caaede1ab867e
MahammadMustafayev <99835006+MahammadMustafayev@users.noreply.github.com>
2022-03-07 09:56:07 +0300

    added

---
+++ TaskMethod(3)/TaskMethod(3)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TaskMethod_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mətn daxil edin:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Burda saitlərin sayı:");
+            Console.WriteLine(VowelLetter(text));           
+        }
+
+        static int VowelLetter(string text)
+        {
+            int count = 0;
+            bool HasA = false;
+            foreach (char letter in text)
+            {
+                if (letter == 'a' || letter == 'ı' || letter=='o' || letter=='u' || letter =='e' || letter=='ə'|| letter=='i'|| letter== 'ö'|| letter=='ü' || letter == 'A' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'E' || letter == 'Ə' || letter == 'İ' || letter == 'Ö' || letter == 'Ü')
+                {
+                    HasA = true;
+                    count++;
+                   
+                }  
+            }
+            return count;
+        }
+    }
+}
